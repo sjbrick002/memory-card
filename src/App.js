2ac48ede8bfbf86b97cb9e139790c1e5c6284f70
SBrick <samuelbrick3@gmail.com>
2022-06-21 21:26:26 -0500

    I removed the 'countries' state property and reimplemented it as a simple array variable. As a replacement, I instead added a 'clickedCountries' state property as an array to keep track of country ids that have been clicked already. Using the clickedCountries state, I was able to finish the updateScore function. The next step is to add styling!

--- src/App.js
+++ src/App.js
@@ -18,92 +18,110 @@ import venezuela from "./images/Venezuela.svg";
 function App() {
   let [score, setScore] = useState(0);
   let [bestScore, setBestScore] = useState(0);
-  let [countries, setCountries] = useState(
-    [
-      {
-        id: 1,
-        imageSrc: argentina,
-        imageAlt: "Flag of Argentina",
-        description: "Flag of Argentina"
-      },
-      {
-        id: 2,
-        imageSrc: bolivia,
-        imageAlt: "Flag of Bolivia",
-        description: "Flag of Bolivia"
-      },
-      {
-        id: 3,
-        imageSrc: brazil,
-        imageAlt: "Flag of Brazil",
-        description: "Flag of Brazil"
-      },
-      {
-        id: 4,
-        imageSrc: chile,
-        imageAlt: "Flag of Chile",
-        description: "Flag of Chile"
-      },
-      {
-        id: 5,
-        imageSrc: colombia,
-        imageAlt: "Flag of Colombia",
-        description: "Flag of Colombia"
-      },
-      {
-        id: 6,
-        imageSrc: ecuador,
-        imageAlt: "Flag of Ecuador",
-        description: "Flag of Ecuador"
-      },
-      {
-        id: 7,
-        imageSrc: guyana,
-        imageAlt: "Flag of Guyana",
-        description: "Flag of Guyana"
-      },
-      {
-        id: 8,
-        imageSrc: paraguay,
-        imageAlt: "Flag of Paraguay",
-        description: "Flag of Paraguay"
-      },
-      {
-        id: 9,
-        imageSrc: peru,
-        imageAlt: "Flag of Peru",
-        description: "Flag of Peru"
-      },
-      {
-        id: 10,
-        imageSrc: suriname,
-        imageAlt: "Flag of Suriname",
-        description: "Flag of Suriname"
-      },
-      {
-        id: 11,
-        imageSrc: uruguay,
-        imageAlt: "Flag of Uruguay",
-        description: "Flag of Uruguay"
-      },
-      {
-        id: 12,
-        imageSrc: venezuela,
-        imageAlt: "Flag of Venezuela",
-        description: "Flag of Venezuela"
-      }
-    ]
-  );
+  let [clickedCountries, setClickedCountries] = useState([]);
+  const countriesArray = [
+    {
+      id: 1,
+      imageSrc: argentina,
+      imageAlt: "Flag of Argentina",
+      description: "Flag of Argentina",
+      clicked: false
+    },
+    {
+      id: 2,
+      imageSrc: bolivia,
+      imageAlt: "Flag of Bolivia",
+      description: "Flag of Bolivia",
+      clicked: false
+    },
+    {
+      id: 3,
+      imageSrc: brazil,
+      imageAlt: "Flag of Brazil",
+      description: "Flag of Brazil",
+      clicked: false
+    },
+    {
+      id: 4,
+      imageSrc: chile,
+      imageAlt: "Flag of Chile",
+      description: "Flag of Chile",
+      clicked: false
+    },
+    {
+      id: 5,
+      imageSrc: colombia,
+      imageAlt: "Flag of Colombia",
+      description: "Flag of Colombia",
+      clicked: false
+    },
+    {
+      id: 6,
+      imageSrc: ecuador,
+      imageAlt: "Flag of Ecuador",
+      description: "Flag of Ecuador",
+      clicked: false
+    },
+    {
+      id: 7,
+      imageSrc: guyana,
+      imageAlt: "Flag of Guyana",
+      description: "Flag of Guyana",
+      clicked: false
+    },
+    {
+      id: 8,
+      imageSrc: paraguay,
+      imageAlt: "Flag of Paraguay",
+      description: "Flag of Paraguay",
+      clicked: false
+    },
+    {
+      id: 9,
+      imageSrc: peru,
+      imageAlt: "Flag of Peru",
+      description: "Flag of Peru",
+      clicked: false
+    },
+    {
+      id: 10,
+      imageSrc: suriname,
+      imageAlt: "Flag of Suriname",
+      description: "Flag of Suriname",
+      clicked: false
+    },
+    {
+      id: 11,
+      imageSrc: uruguay,
+      imageAlt: "Flag of Uruguay",
+      description: "Flag of Uruguay",
+      clicked: false
+    },
+    {
+      id: 12,
+      imageSrc: venezuela,
+      imageAlt: "Flag of Venezuela",
+      description: "Flag of Venezuela",
+      clicked: false
+    }
+  ];
 
-  function updateScore() {
-    setScore(score + 1);
-    if (bestScore < score + 1) {
-      setBestScore(score + 1);
+  function updateScore(id) {
+    if (!clickedCountries.includes(id)) {
+      setScore(score + 1);
+      if (bestScore < score + 1) {
+        setBestScore(score + 1);
+      };
+      setClickedCountries([...clickedCountries,id]);
     };
-    setCountries(shuffleArray(countries));
+    if (clickedCountries.includes(id)) {
+      setScore(0);
+      setClickedCountries([]);
+    };
+    shuffleArray(countriesArray);
   };
 
-  const flagGallery = countries.map(country => <Card key={country.id} onClick={updateScore} imageSrc={country.imageSrc} imageAlt={country.imageAlt} description={country.description}/>);
+  const flagGallery = shuffleArray(countriesArray).map(country => <Card key={country.id} onClick={() => updateScore(country.id)} imageSrc={country.imageSrc} imageAlt={country.imageAlt} description={country.description}/>);
   
   return (
     <div className="App">
