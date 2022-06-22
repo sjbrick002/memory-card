400d001372e772867853ba904c881fc2ac1976be
SBrick <samuelbrick3@gmail.com>
2022-06-21 20:21:29 -0500

    I shortened the file names of each flag svg, and I added a flagGallery card list that displays all 12 cards and their flags

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React, { useState }from "react";
 import shuffleArray from "./shuffleArray";
 import Scoreboard from "./Components/Scoreboard";
 import Card from "./Components/Card";
-import argentina from "./images/Flag_of_Argentina.svg";
-import bolivia from "./images/Flag_of_Bolivia.svg";
-import brazil from "./images/Flag_of_Brazil.svg";
-import chile from "./images/Flag_of_Chile.svg";
-import colombia from "./images/Flag_of_Colombia.svg";
-import ecuador from "./images/Flag_of_Ecuador.svg";
-import guyana from "./images/Flag_of_Guyana.svg";
-import paraguay from "./images/Flag_of_Paraguay.svg";
-import peru from "./images/Flag_of_Peru.svg";
-import suriname from "./images/Flag_of_Suriname.svg";
-import uruguay from "./images/Flag_of_Uruguay.svg";
-import venezuela from "./images/Flag_of_Venezuela.svg";
+import argentina from "./images/Argentina.svg";
+import bolivia from "./images/Bolivia.svg";
+import brazil from "./images/Brazil.svg";
+import chile from "./images/Chile.svg";
+import colombia from "./images/Colombia.svg";
+import ecuador from "./images/Ecuador.svg";
+import guyana from "./images/Guyana.svg";
+import paraguay from "./images/Paraguay.svg";
+import peru from "./images/Peru.svg";
+import suriname from "./images/Suriname.svg";
+import uruguay from "./images/Uruguay.svg";
+import venezuela from "./images/Venezuela.svg";
 
 function App() {
   let [score, setScore] = useState(0);
@@ -99,12 +99,12 @@ function App() {
     setCountries(shuffleArray(countries));
   };
 
-  let country = countries[0];
+  const flagGallery = countries.map(country => <Card key={country.id} onClick={shuffleCountries} imageSrc={country.imageSrc} imageAlt={country.imageAlt} description={country.description}/>)
   
   return (
     <div className="App">
       <Scoreboard score={score} bestScore={bestScore}/>
-      <Card onClick={shuffleCountries} imageSrc={country.imageSrc} imageAlt={country.imageAlt} description={country.description}/>
+      {flagGallery}
     </div>
   );
 }
