e7134ced2cddad00f62d2a817a617cb9f0685fe7
SBrick <samuelbrick3@gmail.com>
2022-06-22 11:17:13 -0500

    I added styling to the Card component.

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,6 +1,8 @@
+import "./Card.css";
+
 export default function Card(props) {
     return (
-        <div onClick={props.onClick}>
+        <div onClick={props.onClick} className="card-body">
             <img src={props.imageSrc} alt={props.imageAlt}/>
             <p>{props.description}</p>
         </div>
